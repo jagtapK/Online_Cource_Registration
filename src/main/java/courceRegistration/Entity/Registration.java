d3b208ccdfd4b32884e5b5e6aac5acfeda66ba26
jagtapK <jagtapkajal54@gmail.com>
2025-07-21 13:55:25 +0530

    new commit

--- src/main/java/courceRegistration/Entity/Registration.java
+++ src/main/java/courceRegistration/Entity/Registration.java
@@ -1,5 +1,69 @@
 package courceRegistration.Entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "registration")
 public class Registration {
 
+    @Id
+	private int id;
+	private String studentName;
+	
+	private String courceName;
+	private Date registrationDate;
+	
+	private double feesPaid;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getCourceName() {
+		return courceName;
+	}
+
+	public void setCourceName(String courceName) {
+		this.courceName = courceName;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public double getFeesPaid() {
+		return feesPaid;
+	}
+
+	public void setFeesPaid(double feesPaid) {
+		this.feesPaid = feesPaid;
+	}
+
+	@Override
+	public String toString() {
+		return "Registration [id=" + id + ", studentName=" + studentName + ", courceName=" + courceName
+				+ ", registrationDate=" + registrationDate + ", feesPaid=" + feesPaid + "]";
+	}
+	
+	
 }
