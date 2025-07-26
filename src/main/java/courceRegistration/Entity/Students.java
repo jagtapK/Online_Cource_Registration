b8b30f7a06f7fb7f3b545a15c4b29eaf20b5fbcc
jagtapK <jagtapkajal54@gmail.com>
2025-07-26 23:52:16 +0530

    Added Student Details

--- src/main/java/courceRegistration/Entity/Students.java
+++ src/main/java/courceRegistration/Entity/Students.java
@@ -1,5 +1,61 @@
 package courceRegistration.Entity;
 
+import hibernateMapping.Entity.Document;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
 public class Students {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String fname;
+
+	private String lname;
+	private String Department;
+
+	private int mobileNo;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getDepartment() {
+		return Department;
+	}
+
+	public void setDepartment(String department) {
+		Department = department;
+	}
+
+	public int getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	@Override
+	public String toString() {
+		return "student [id=" + id + ", fname=" + fname + ", lname=" + lname + ", Department=" + Department
+				+ ", mobileNo=" + mobileNo + "]";
+	}
 
 }
